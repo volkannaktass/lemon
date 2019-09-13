bf9defaf9fc173e8d404ed649b229266c406b247
Volkan Aktas <volkanaktas98@gmail.com>
2019-09-13 20:49:17 +0300

    update all files,article and user models

---
+++ article/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = "article"
+urlpatterns = [
+   # path('create/',views.index,name = "index"),
+   path('dashboard/',views.dashboard,name = "dashboard"),
+   path('addarticle/',views.addArticle,name = "addarticle"),
+   path('article/<int:id>',views.detail,name = "detail"),
+   path('update/<int:id>',views.updateArticle,name = "update"),
+   path('delete/<int:id>',views.deleteArticle,name = "delete"),
+   path('',views.articles,name = "articles"),
+   path('comment/<int:id>',views.addComment,name = "comment"),
+   #path('deletecomment/<int:id>',views.deleteComment,name = "deletecomment"),
+]
---
+++ user/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+#from django.contrib.auth.models import User
+# Create your models here.
+#from django.contrib.auth.models import (
+  #  BaseUserManager, AbstractBaseUser
+)
+
+
+
+#class UserProfile(models.Model): # kullanıcıların diğer bilgileri
+    #user = models.OneToOneField(User, on_delete=models.CASCADE) 
+    #pp = models.BooleanField(default = False,verbose_name = "profil resmi")
+    #is_author = models.BooleanField(default = True, verbose_name = "yazar olarak kabul et") 
+    #author = models.BooleanField(default = True, verbose_name = "yazarlık başvurusu")
+    #about = RichTextField(null = True, blank = True,verbose_name = "kişi hakkında")
+    #following = models.IntegerField(default = 0)
+    #followers = models.IntegerField(default = 0)
+    #hmanycontent = models.IntegerField(default = 0)
